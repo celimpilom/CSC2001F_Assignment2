afcab0cb8537d7f126413c08bba4ac3c81d6f3b5
celimpilo <manqelecm7@gmail.com>
2021-04-22 21:14:09 +0200

    AccessAVLApp runner class created

---
+++ src/AccessAVLApp.java
@@ -0,0 +1,37 @@
+import java.io.FileNotFoundException;
+
+public class AccessAVLApp {
+
+    static AVLTree<String>   tree;
+    public static void main(String[] args ) throws FileNotFoundException{
+
+        ReadFile object = new ReadFile();
+        object.tree();
+        tree = object.getTree();
+        
+        if (args.length == 1){
+            String studentID = args[0];
+            System.out.println(printStudent(studentID)  );
+            System.out.println(tree.opCount);
+        } else {
+            printAll();
+ 
+        }
+          
+    }
+    static String printStudent(String studentID){
+        BinaryTreeNode<String> foundNode = tree.find(studentID);
+            if (foundNode != null){
+                String[] b = foundNode.data.split(" ");
+                return b[1] + " " + b[2];
+            }
+           
+        return "Access denied!";
+            } 
+    static void printAll(){
+        tree.treeOrder();
+    }
+}
+
+    
+
